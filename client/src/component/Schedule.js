f8b7cf70b6d0a1323d2308fabeb2cedd73ceb8ff
sinyoung98 <joyhope3@naver.com>
2020-10-11 18:56:40 +0900

    [ADD]schedule 틀 생성하였습니다.

--- client/src/component/Schedule.js
+++ client/src/component/Schedule.js
@@ -5,12 +5,11 @@ import axios from "axios"
 export default function Schedule(props){
     const {userId} = props;
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([{id : 18, date: "20-39-2", gift: "wow"}]);
 
     useEffect(() => {
-        const url = `http://ec2-3-34-177-67.ap-northeast-2.compute.amazonaws.com:5000/schedule/${userId}`
-        console.log(url);
-        axios.get(url)
+
+        axios.get(`http://ec2-3-34-177-67.ap-northeast-2.compute.amazonaws.com:5000/schedule/${window.sessionStorage.getItem("id")}`)
         .then((res) => {
           console.log(res.data);
           setData(res.data);
@@ -20,7 +19,16 @@ export default function Schedule(props){
     return(
         <div className="schedule">
             <h1>Schedule</h1>
-            
+            <button>+</button>
+            <ul className="schedule_list">
+                {
+                    data && data.map((data) => {
+                        return (
+                        <li key={data.id}>{data.id}, {data.date}, {data.gift}</li>
+                        )
+                    })
+                }
+                </ul>
             </div>
     )
 }
