c8c20016df4bf8fc31cc6c281c4195f9f5cf6514
민재 KIM <62498196+bapbodanbbang@users.noreply.github.com>
2020-10-13 17:08:14 +0900

    Update Kakao.js

--- client/src/component/Kakao.js
+++ client/src/component/Kakao.js
@@ -15,22 +15,20 @@ class KakaoSignUp extends Component {
             data: res
         })
         axios.post('https://www.don-forget.com/user/signin', {
-            socialLogin: 'kakao',
-            email: this.state.data.profile.kakao_account.email,
+            email: 'kakao',
             name: this.state.data.profile.properties.nickname
         })
-            .then((response) => response.data)
-            .then((response) => {
-                window.sessionStorage.setItem("id", response.id);
-                window.sessionStorage.setItem("email", response.email);
-                window.sessionStorage.setItem("name", response.name);
-                this.props.setEmail(response.email);
-                this.props.setName(response.name);
-                this.props.setUserId(response.id);
-                this.props.setIsLogin(true);
-            })
-            .catch((err) => console.log(err));
-        console.log(this.state.data)
+        .then((response) => response.data)
+        .then((response) => {
+            window.sessionStorage.setItem("id", response.id);
+            window.sessionStorage.setItem("email", response.email);
+            window.sessionStorage.setItem("name", response.name);
+            this.props.setEmail(response.email);
+            this.props.setName(response.name);
+            this.props.setUserId(response.id);
+            this.props.setIsLogin(true);
+        })
+        .catch((err) => console.log(err));
     }
     responseFail = (err) => {
         alert(err);
