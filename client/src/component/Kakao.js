47a7eeecaf918aed5dee9f70015477d17f6321d9
김호정 <hjnk125@gmail.com>
2020-10-14 16:33:44 +0900

    [FIX]Kakao로그인

--- client/src/component/Kakao.js
+++ client/src/component/Kakao.js
@@ -1,40 +1,38 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import KaKaoLogin from 'react-kakao-login';
+export default function KakaoSignUp() {
+  function responseKaKao(res) {
+    axios.post('https://don-forget-server.com/user/signin', {
+      email: ['kakao', res.profile.properties.nickname],
+      name: res.profile.properties.nickname
+    })
 
-export default function KakaoSignUp(){
-    function responseKaKao(res){
-        axios.post('https://don-forget-server.com/user/signin', {
-            email: 'kakao',
-            name: res.profile.properties.nickname
-        })
-            .then((response) => response.data)
-            .then((response) => {
-                window.sessionStorage.setItem("id", response.id);
-                window.sessionStorage.setItem("email", response.email);
-                window.sessionStorage.setItem("name", response.name);
-                this.props.setEmail(response.email);
-                this.props.setName(response.name);
-                this.props.setUserId(response.id);
-                this.props.setIsLogin(true);
-            })
-            .catch((err) => console.log(err));
-    }
-
-    function responseFail(err){
-        alert(err);
-    }
-        return (
-            <>
-                <KaKaoLogin
-                    jsKey={'d7fcccce457540e5621e96787fac52a9'}
-                    buttonText="KaKao"
-                    onSuccess={responseKaKao}
-                    onFailure={responseFail}
-                    getProfile={true}
-                    useDefaultStyle={true}
-                />
-            </>
-        );
+      .then((response) => response.data)
+      .then((response) => {
+        window.sessionStorage.setItem("id", response.id);
+        window.sessionStorage.setItem("email", response.email);
+        window.sessionStorage.setItem("name", response.name);
+        this.props.setEmail(response.email);
+        this.props.setName(response.name);
+        this.props.setUserId(response.id);
+        this.props.setIsLogin(true);
+      })
+      .catch((err) => console.log(err));
+  }
+  function responseFail(err) {
+    alert(err);
+  }
+  return (
+    <>
+      <KaKaoLogin
+        jsKey={'d7fcccce457540e5621e96787fac52a9'}
+        buttonText="KaKao"
+        onSuccess={responseKaKao}
+        onFailure={responseFail}
+        getProfile={true}
+        useDefaultStyle={true}
+      />
+    </>
+  );
 }
-
