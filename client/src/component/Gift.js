a2f1970cefc9ef40ab61de8d1a04d82159e1aef3
김호정 <hjnk125@gmail.com>
2020-10-27 14:03:03 +0900

    [FIX]replaceAll

--- client/src/component/Gift.js
+++ client/src/component/Gift.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from "react-router-dom";
 import './Gift.scss';
+import Avatar from '@material-ui/core/Avatar';
+import SearchIcon from '@material-ui/icons/Search';
 import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
@@ -27,7 +29,7 @@ function Gift() {
 
   const [isSearchingEmoticon, setIsSearchingEmoticon] = useState(false);
   const [emoticon, setEmoticon] = useState([]);
-  const [CopyLink,setCopyLink] = useState("");
+  const [CopyLink, setCopyLink] = useState("");
 
   const updateWidth = () => {
     if (window.innerWidth < 757) {
@@ -195,14 +197,14 @@ function Gift() {
                       </div>
                       <div className="copyIconDiv">
                         <CopyToClipboard
-                          style={{ fontSize : 25, color : "#af9eed" }} 
+                          style={{ fontSize: 25, color: "#af9eed" }}
                           text={`https://e.kakao.com/t/${data.titleUrl}`}
-                          onCopy={() => {setCopyLink(i)}}
+                          onCopy={() => { setCopyLink(i) }}
                         >
-                          {CopyLink === i ? <CheckCircleIcon/> : <CheckCircleOutlineIcon/>}
+                          {CopyLink === i ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />}
                         </CopyToClipboard>
-                        <div className="copyText" style={{ color : "#af9eed" }}>
-                        {CopyLink === i ? 'Copy' : 'Link'}
+                        <div className="copyText" style={{ color: "#af9eed" }}>
+                          {CopyLink === i ? 'Copy' : 'Link'}
                         </div>
                       </div>
                     </div>
